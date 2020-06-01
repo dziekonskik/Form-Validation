b3a7f283c124a06e6d818298e6231f6bb2937244
Seven <jaseveen@gmail.com>
2020-06-01 21:00:22 +0200

    add finished validation

--- form.js
+++ form.js
@@ -1,6 +1,6 @@
 const errorParagraphs = Array.from(document.querySelectorAll('.form__error'));
 
-function addError(message, errorParagraphIndex) {
+function addErrorClass(message, errorParagraphIndex) {
   errorParagraphs.forEach((paragraph, index) => {
     if (index === errorParagraphIndex) {
       paragraph.innerHTML = message;
@@ -9,64 +9,76 @@ function addError(message, errorParagraphIndex) {
   });
 }
 
+function removeErrorClass(pararaphList) {
+  pararaphList.forEach((p) => {
+    p.classList.remove('error');
+    p.innerHTML = '';
+  });
+}
+
 function validateName(name) {
   if (name.length < 3) {
-    addError('Name must be at least 2 characters', 0);
+    addErrorClass('Name must be at least 3 characters', 0);
+  }
+  if (/\d|\W/.test(name)) {
+    addErrorClass('Name must contain letters only', 0);
   } else {
-    errorParagraphs.forEach((p) => {
-      p.classList.remove('error');
-      p.innerHTML = '';
-    });
+    removeErrorClass(errorParagraphs);
   }
 }
 
 function validateEmail(email) {
   if (!email.toString().includes('@')) {
-    addError('Make sure your email has @ character', 1);
+    addErrorClass('Make sure your email has @ character', 1);
   }
   if (!/\.\w{2,3}$/.test(email)) {
-    addError('Make sure your email has domain', 1);
+    addErrorClass('Make sure your email has domain', 1);
   }
 }
 
 function validatePassword(password) {
   if ([...password].length < 9) {
-    addError('Make sure your password is at least 8 characters long', 2);
+    addErrorClass('Make sure your password is at least 8 characters long', 2);
   }
   if (!/[A-Z]/.test(password)) {
-    addError('Password must contain an uppercase letter', 2);
+    addErrorClass('Password must contain an uppercase letter', 2);
   }
   if (!/[0-9]/.test(password)) {
-    addError('Password must contain a number', 2);
+    addErrorClass('Password must contain a number', 2);
+  }
+  if (!/\W/.test(password)) {
+    addErrorClass('Password must contain a non alphanumeric character', 2);
   }
 }
 
 function confirmPassword(password, passwordToCheck) {
-  return password === passwordToCheck
-    ? true
-    : addError('Password does not match', 3);
+  if (password !== passwordToCheck) addErrorClass('Password does not match', 3);
 }
 
 function validateCheckbox(checkbox) {
-  if (checkbox.checked) {
-    console.log('czek');
-  } else {
-    console.length('nieczek');
+  if (!checkbox.checked) {
+    addErrorClass('You must agree to GDPR rules', 4);
   }
 }
 
 function submitFormData(e) {
-  e.preventDefault();
+  //e.preventDefault();
   const nameInput = document.querySelector('#name');
   const emailInput = document.querySelector('#email');
   const passwordInput = document.querySelector('#password');
   const confirmInput = document.querySelector('#confirm');
   const checkbox = document.querySelector('#check');
-  validateName(nameInput.value);
-  validateEmail(emailInput.value);
-  validatePassword(passwordInput.value);
-  confirmPassword(passwordInput.value, confirmInput.value);
-  console.log(passwordInput.value.length);
+  if (
+    validateName(nameInput.value) &&
+    validateEmail(emailInput.value) &&
+    validatePassword(passwordInput.value) &&
+    confirmPassword(passwordInput.value, confirmInput.value) &&
+    validateCheckbox(checkbox)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 document.forms[0].addEventListener('submit', submitFormData);
